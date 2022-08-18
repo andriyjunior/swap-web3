0d2e9b81717ff2433fabc75f097290208f44469e
andriyjunior <andriyjunior14@gmail.com>
2022-08-18 11:56:35 +0300

    feat: refactoring metaMask connecting, handle onChangeAccounts

--- src/hooks/useMetaMask.tsx
+++ src/hooks/useMetaMask.tsx
@@ -16,6 +16,7 @@ import {
 } from 'store'
 
 const ETH_REQUESTACCOUNTS = 'eth_requestAccounts'
+const ACCOUNTS_CHANGED = 'accountsChanged'
 
 const initialState = {
   accountAddress: '',
@@ -27,12 +28,16 @@ export const useMetamaskConnection = () => {
   const user = useAppSelector(selectUser)
 
   const [accountInfo, setAccountInfo] = useState(initialState)
-
   const [isConnected, setIsConnected] = useState(false)
 
   const { ethereum } = window
 
   const provider = ethereum && new ethers.providers.Web3Provider(ethereum)
+
+  const getBalance = async (address: string) => {
+    const bal = (await provider?.getBalance(address)) || ''
+    return ethers.utils.formatEther(bal)
+  }
 
   const connect = async () => {
     try {
@@ -44,10 +49,9 @@ export const useMetamaskConnection = () => {
         ethereum?.request &&
         (await ethereum?.request({ method: ETH_REQUESTACCOUNTS }))
 
-      const balance = await provider?.getBalance(accounts[0])
-      const bal = ethers.utils.formatEther(balance ?? '')
+      const balance = await getBalance(accounts[0])
 
-      setAccountInfo({ accountAddress: accounts[0], accountBalance: bal })
+      setAccountInfo({ accountAddress: accounts[0], accountBalance: balance })
       setIsConnected(true)
     } catch (error) {
       setIsConnected(false)
@@ -71,6 +75,21 @@ export const useMetamaskConnection = () => {
     setIsConnected(false)
     dispatch(logOut())
   }
+
+  const handleOnAccountsChange = async (accounts: string[]) => {
+    if (!isConnected) return
+
+    if (!accounts.length) {
+      disconnect()
+    }
+
+    const newAccount = accounts[0]
+    const newBal = await getBalance(newAccount)
+
+    setAccountInfo({ accountAddress: newAccount, accountBalance: newBal })
+  }
+
+  ethereum?.on(ACCOUNTS_CHANGED, handleOnAccountsChange)
 
   return { connect, disconnect }
 }
