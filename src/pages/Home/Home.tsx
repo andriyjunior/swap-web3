138e1ecebb1cea1e587e27310acec5477d3633b2
andriyjunior <andriyjunior14@gmail.com>
2022-08-31 13:07:48 +0300

    feat: add range

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,16 @@
-import { FC } from 'react'
+import { Range } from 'components'
+import { FC, useState } from 'react'
 
 export const Home: FC = () => {
-  return <></>
+  const [range, setRange] = useState(0)
+
+  return (
+    <>
+      <Range
+        name="amount"
+        value={range}
+        onChange={(value) => setRange(value)}
+      />
+    </>
+  )
 }
